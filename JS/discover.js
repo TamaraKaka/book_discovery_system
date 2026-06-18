99947f954035f94e6a0ef5a7cdd3bf08d5152c13
TamaraKaka <kaka.tamara@strathmore.edu>
2026-06-18 11:48:46 +0300

    Added if else clause for search

--- JS/discover.js
+++ JS/discover.js
@@ -14,21 +14,33 @@ genreCards.forEach(card => {
 function normalize(str) {
   return str.trim().toLowerCase();
 }
-const input=document.getElementById("searchInput");
-const searchButton=document.getElementById("searchBtn");
+const input = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchBtn");
 const container = document.getElementById("booksContainer");
-searchButton.addEventListener('click',()=>{
-const filteredBooks = books.filter(book => normalize(book.author) === normalize(input.value) 
-|| normalize(book.title) === normalize(input.value) );
-container.innerHTML = filteredBooks.map(book => `
-    <div class="col">
-        <div class="card h-100 text-center shadow-sm">
-            <img src="${book.image}" class="card-img-top book-img">
-            <div class="card-body">
-                <h6 class="card-title">${book.title}</h6>
+
+searchButton.addEventListener("click", () => {
+
+    const filteredBooks = books.filter(book =>
+        normalize(book.author) === normalize(input.value) ||
+        normalize(book.title) === normalize(input.value)
+    );
+
+    if (filteredBooks.length > 0) {
+
+        container.innerHTML = filteredBooks.map(book => `
+            <div class="col">
+                <a href="about.html?title=${encodeURIComponent(book.title)}">
+                    <div class="card h-100 text-center shadow-sm">
+                        <img src="${book.image}" class="card-img-top book-img">
+                        <div class="card-body">
+                            <h6 class="card-title">${book.title}</h6>
+                        </div>
+                    </div>
+                </a>
             </div>
-        </div>
-    </div>
-`).join("")
-}
-);
+        `).join("");
+
+    } else {
+        container.innerHTML = `<h3>No books under that title or author</h3>`;
+    }
+});
