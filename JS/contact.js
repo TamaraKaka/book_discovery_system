dc0963c614c87f739fe07633bc6d9279d5645bc4
TamaraKaka <kaka.tamara@strathmore.edu>
2026-06-25 20:42:53 +0300

    Added java functionality to contact form

--- JS/contact.js
+++ JS/contact.js
@@ -1,6 +1,30 @@
 let bio=document.getElementById("floatingTextarea2");
 let count=document.getElementById("count");
+let form=document.getElementById("contactForm");
+
 
 bio.addEventListener("input", () => {
     count.textContent = `${bio.value.length}/200`;
+});
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    document.getElementById("message").textContent =
+        "✓ Thank you for your feedback!";
+
+    setTimeout(() => {
+        form.reset();
+        document.getElementById("message").textContent = "";
+    }, 2000);
+});
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    document.getElementById("message").textContent =
+        "✓ Thank you for your feedback!";
+
+    setTimeout(() => {
+        form.reset();
+    }, 2000);
 });
